bc2debb50a69c2e8a1e880a5382fa837933e22d3
De La Rosa <mdelares@outlook.com>
2015-11-17 21:05:54 -0600

    adding classes

---
+++ Product_CSharpFive.cs
@@ -0,0 +1,51 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace CSharp5
+{
+    public class Product
+    {
+        public long Id { get; private set; }
+        public string Name { get; set; }
+        public string LongName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string[] OtherNames { get; set; }
+
+
+        public Product(string name)
+        {
+            Id = GenerateId();
+            Name = name;
+            LongName = Id + " - " + Name;
+            IsActive = true;
+            ExpirationDate = DateTime.Now.AddYears(1);
+        }
+
+        private long GenerateId()
+        {
+            Random random = new Random();
+            return random.Next(0, 1000000);
+        }
+
+
+        public void ExtendExpirationDate(int numberOfMonths)
+        {
+            ExpirationDate = ExpirationDate.AddMonths(numberOfMonths);
+        }
+
+        public bool IsExpired()
+        {
+            return (DateTime.Now > ExpirationDate);
+        }
+
+
+        public string GetFirstOtherName()
+        {
+            return OtherNames != null ? OtherNames[0] : null;
+        }
+    }
+}
---
+++ Product_CSharpSix.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace CSharp6
+{
+    public class Product
+    {
+        public long Id { get; } = GenerateId();
+        public string Name { get; }
+
+        public string[] OtherNames { get; set; }
+        public string LongName => Id + " - " + Name;
+        public bool IsActive { get; set; } = true;
+        public DateTime ExpirationDate { get; set; } = DateTime.Now.AddYears(1);
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+
+
+        private static long GenerateId()
+        {
+            Random random = new Random();
+            return random.Next(0, 1000000);
+        }
+
+        public void ExtendExpirationDate(int numberOfMonths)
+            => ExpirationDate = ExpirationDate.AddMonths(numberOfMonths);
+
+        public bool IsExpired() => (DateTime.Now > ExpirationDate);
+
+        public string GetFirstOtherName()
+        {
+            return OtherNames?[0];
+        }
+    }
+}
